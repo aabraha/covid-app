e90a3c63bf4594d895610bf16422623c79888bc7
Assefa <assefamit@gamil.com>
2020-08-13 11:54:41 -0500

    properties modified to reset default username and password

--- src/main/java/com/project/covidapp/CovidAppApplication.java
+++ src/main/java/com/project/covidapp/CovidAppApplication.java
@@ -28,27 +28,31 @@ public class CovidAppApplication {
 		SpringApplication.run(CovidAppApplication.class, args);
 		
 		// auto consume data
-		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/new_cases_by_day.json";
-		ConsumedModel dto = restTemplate.getForObject(url, ConsumedModel.class);
-		
-		//parse the data
-		System.err.println("\n============the whole data object==========");
-		System.out.println("the whole data: " + dto);
-		
-		System.out.println("\n============the columns field============");
-		System.out.println("the columns:" + dto.getData().getColumns());
-		
-		System.out.println("\n=============the columns field list==============");
-		System.out.println("the X part:\n" + dto.getData().getColumns().get(0));
-		System.out.println("the Data1 part:\n" + dto.getData().getColumns().get(1));
-		System.out.println("the Data2 part:\n" + dto.getData().getColumns().get(2));
-		
-		System.out.println("\n============the columns field inner list============");
-		System.out.println("the X part:\n" + dto.getData().getColumns().get(0).get(0));
-		System.out.println("the Data1 part:\n" + dto.getData().getColumns().get(1).get(0));
-		System.out.println("the Data2 part:\n" + dto.getData().getColumns().get(2).get(0));
-
+		/*
+		 * RestTemplate restTemplate = new RestTemplate(); String url =
+		 * "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/new_cases_by_day.json";
+		 * ConsumedModel dto = restTemplate.getForObject(url, ConsumedModel.class);
+		 * 
+		 * //parse the data
+		 * System.err.println("\n============the whole data object==========");
+		 * System.out.println("the whole data: " + dto);
+		 * 
+		 * System.out.println("\n============the columns field============");
+		 * System.out.println("the columns:" + dto.getData().getColumns());
+		 * 
+		 * System.out.println("\n=============the columns field list==============");
+		 * System.out.println("the X part:\n" + dto.getData().getColumns().get(0));
+		 * System.out.println("the Data1 part:\n" + dto.getData().getColumns().get(1));
+		 * System.out.println("the Data2 part:\n" + dto.getData().getColumns().get(2));
+		 * 
+		 * System.out.println("\n============the columns field inner list============");
+		 * System.out.println("the X part:\n" +
+		 * dto.getData().getColumns().get(0).get(0));
+		 * System.out.println("the Data1 part:\n" +
+		 * dto.getData().getColumns().get(1).get(0));
+		 * System.out.println("the Data2 part:\n" +
+		 * dto.getData().getColumns().get(2).get(0));
+		 */
 	}
 	
 	//RestTemplate Bean
